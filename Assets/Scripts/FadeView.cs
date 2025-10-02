378aee35d64ab96c07aa3e74a21abe8e00362a36
K.Hamada <k.hamadas55@gmail.com>
2025-10-02 18:18:49 +0900

    FadeViewを追加する。

---
+++ Assets/Scripts/FadeView.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using LitMotion;
+using LitMotion.Extensions;
+using Cysharp.Threading.Tasks;
+
+public class FadeView : MonoBehaviour
+{
+    [SerializeField] private Image _fadeImage;
+    [SerializeField] private float _fadeTime;
+
+    public bool Visible { get { return gameObject.activeSelf; } set { gameObject.SetActive(value); } }
+
+    public async UniTask FadeInAsync()
+    {
+        gameObject.SetActive(true);
+        _fadeImage.gameObject.SetActive(true);
+        _fadeImage.color = new Color(1, 1, 1, 0);
+
+        var motion = LMotion.Create(_fadeImage.color, new Color(1, 1, 1, 1), _fadeTime)
+            .WithEase(Ease.Linear)
+            .BindToColor(_fadeImage);
+        await motion.ToUniTask();
+    }
+
+    public async UniTask FadeOutAsync()
+    {
+        gameObject.SetActive(true);
+        _fadeImage.gameObject.SetActive(true);
+        _fadeImage.color = new Color(1, 1, 1, 1);
+
+        var motion = LMotion.Create(_fadeImage.color, new Color(1, 1, 1, 0), _fadeTime)
+            .WithEase(Ease.Linear)
+            .BindToColor(_fadeImage);
+        await motion.ToUniTask();
+    }
+}
