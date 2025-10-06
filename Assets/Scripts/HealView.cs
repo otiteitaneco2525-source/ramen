d39cd52ceef385047ce1cf6af0197ffe064f7355
K.Hamada <k.hamadas55@gmail.com>
2025-10-06 22:23:08 +0900

    回復の演出を修正する。

--- Assets/Scripts/HealView.cs
+++ Assets/Scripts/HealView.cs
@@ -4,15 +4,16 @@ using UnityEngine.UI;
 using LitMotion;
 using LitMotion.Extensions;
 using UnityEngine.Events;
+using TMPro;
 
 public class HealView : MonoBehaviour
 {
     [SerializeField] private Image _backgroundImage;
     [SerializeField] private Image _frontFrontImage;
     [SerializeField] private Image _frontImage;
-    [SerializeField] private Image _character1Image;
-    [SerializeField] private Image _character2mage;
     [SerializeField] private Button _closeButton;
+    [SerializeField] private TextMeshProUGUI _character1Text;
+    [SerializeField] private TextMeshProUGUI _character2Text;
 
     public bool Visible { get { return gameObject.activeSelf; } set { gameObject.SetActive(value); } }
     public UnityAction OnCloseButtonClicked { get; set; }
@@ -28,9 +29,9 @@ public class HealView : MonoBehaviour
         _frontImage.gameObject.SetActive(false);
         _frontImage.transform.localPosition = new Vector3(0, 1080, 0);
         _frontFrontImage.gameObject.SetActive(false);
-        _character1Image.gameObject.SetActive(false);
-        _character2mage.gameObject.SetActive(false);
         _closeButton.gameObject.SetActive(false);
+        _character1Text.gameObject.SetActive(false);
+        _character2Text.gameObject.SetActive(false);
 
         var backgroundImageFromColor = _backgroundImage.color;
         backgroundImageFromColor.a = 0;
@@ -58,29 +59,29 @@ public class HealView : MonoBehaviour
             .BindToColor(_frontFrontImage);
         await motion3.ToUniTask();
 
-        _character1Image.gameObject.SetActive(true);
-        var character1ImageColor = _character1Image.color;
-        character1ImageColor.a = 0;
-        var character1ImageToColor = character1ImageColor;
-        character1ImageToColor.a = 1;
-        var motion4 = LMotion.Create(character1ImageColor, character1ImageToColor, 0.25f)
+        _character1Text.gameObject.SetActive(true);
+        var character1TextColor = _character1Text.color;
+        character1TextColor.a = 0;
+        var character1TextToColor = character1TextColor;
+        character1TextToColor.a = 1;
+        var motion5 = LMotion.Create(character1TextColor, character1TextToColor, 0.25f)
             .WithEase(Ease.Linear)
-            .BindToColor(_character1Image);
-        await motion4.ToUniTask();
+            .BindToColor(_character1Text);
+        await motion5.ToUniTask();
 
         await UniTask.Delay(1500);
 
-        _character1Image.gameObject.SetActive(false);
+        _character1Text.gameObject.SetActive(false);
 
-        _character2mage.gameObject.SetActive(true);
-        var character2mageColor = _character2mage.color;
-        character2mageColor.a = 0;
-        var character2mageToColor = character2mageColor;
-        character2mageToColor.a = 1;
-        var motion5 = LMotion.Create(character2mageColor, character2mageToColor, 0.25f)
+        _character2Text.gameObject.SetActive(true);
+        var character2TextColor = _character2Text.color;
+        character2TextColor.a = 0;
+        var character2TextToColor = character2TextColor;
+        character2TextToColor.a = 1;
+        var motion6 = LMotion.Create(character2TextColor, character2TextToColor, 0.25f)
             .WithEase(Ease.Linear)
-            .BindToColor(_character2mage);
-        await motion5.ToUniTask();
+            .BindToColor(_character2Text);
+        await motion6.ToUniTask();
 
         _closeButton.gameObject.SetActive(true);
     }
